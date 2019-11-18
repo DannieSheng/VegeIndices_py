ef19b26cf61ec2fce110738884879e2de91bb2c7
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2019-11-18 11:49:49 -0500

    Create script_plot_greenup.py

---
+++ script_plot_greenup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 11:44:27 2019
+A script to plot the green up data on top of the original image
+@author: hdysheng
+"""
+
+import os
+import pandas as pd
+
+path = r'T:\AnalysisDroneData\ReflectanceCube\indices\CLMB GWAS 2019 Flight Data\100083_2019_06_25_15_59_59'
+name = ''
