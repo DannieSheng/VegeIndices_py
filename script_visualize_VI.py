df649f589c9c2292e118f1aea292c4a638ba2e1f
Hudanyun Sheng <hudanyun.sheng@gmail.com>
2019-11-18 11:49:15 -0500

    Update script_visualize_VI.py

--- script_visualize_VI.py
+++ script_visualize_VI.py
@@ -12,30 +12,50 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import pylab as pl
 import pdb
-plt.close('all')
-(?# path_greenup = r'T:\AnalysisDroneData\ReflectanceCube\indices\CLMB STND 2019 Flight Data\greenup_related')
-# name_greenup = 'CLMB_STND_2019_Master Data_Working.xlsx'
-path_VI      = r'T:\AnalysisDroneData\ReflectanceCube\indices\CLMB STND 2019 Flight Data\100081_2019_06_11_17_57_06'
-name_VI      = 'VI_summary_greenup.csv'
-df_VI        = pd.read_csv(os.path.join(path_VI, name_VI), encoding = 'Latin-1')
 
-list_geno = df_greenup['GENO'].unique()
-greenup_rate = ['first green tiller', '50% crown green up', '100% crown green up']
-#fig, axs = plt.subplots(2,3)
-#axs = axs.ravel()
-for (idx, geno) in enumerate(list_geno):
-    cond = df_greenup['GENO'] == geno
-    df_geno = df_greenup.loc[cond,:]
-    list_loc = df_geno['PLOT_LC']
-    fig = plt.figure()
-    for loc in list_loc:
-        cond1 = df_geno['PLOT_LC'] == loc
-#        gr = df_geno.loc[cond1, ['GREEN1', 'GREEN50', 'GREEN100']].values.tolist()[0]
-        gr = df_geno.loc[cond1, ['Unnamed: 7', 'Unnamed: 9', 'Unnamed: 11']].astype(datetime).values.tolist()[0]
-        plt.plot(gr, greenup_rate, '^-', label = loc)
-#        axs[idx].plot(gr, greenup_rate, '^-', label = loc)
-    plt.title('Green up information for {}'.format(geno))
-    plt.legend(loc = 'best')
-    pl.xticks(rotation=45)
-    pl.yticks(rotation=45)
-#    pdb.set_trace()
+plt.close('all')
+df_all = dict()
+list_dates   = ['100081_2019_06_11_17_57_06', '100084_2019_06_25_16_39_57', '100085_2019_07_18_15_54_58']
+list_dates_  = ['2019_06_11', '2019_06_25', '2019_07_18']
+name_VI      = 'VI_summary_greenup.csv'
+savepath     = r'T:\AnalysisDroneData\ReflectanceCube\indices\CLMB STND 2019 Flight Data\greenup_related'
+
+list_VI  = ['ACI', 'ARI', 'CARI', 'CI_red_edge', 'EVI', 'MARI', 'MCARI', 'MTCI', 'NDVI', 'PRI', 'RGRI', 'RVSI', 'SIPI', 'SR', 'VARI', 'VI_green', 'WBI']
+for date in list_dates:
+    path_VI      = r'T:\AnalysisDroneData\ReflectanceCube\indices\CLMB STND 2019 Flight Data\{}'.format(date)
+    df_VI        = pd.read_csv(os.path.join(path_VI, name_VI), encoding = 'Latin-1')
+    df_all[date] = df_VI
+list_geno = df_VI['GENO'].unique() 
+for VI in list_VI:
+    for (idx, date) in enumerate(list_dates):
+        df_temp = df_all[date][['PLOT_GL', VI, 'PLOT_LC', 'GENO']]
+        if idx == 0:
+            df_VIdate = df_temp
+            df_VIdate = df_VIdate.rename(columns = {VI: date})
+#            pdb.set_trace()
+        else:
+            df_VIdate = df_VIdate.merge(df_temp, how = 'inner', on = ['PLOT_GL', 'PLOT_LC', 'GENO'])
+            df_VIdate = df_VIdate.rename(columns = {VI: date})
+    
+    for (idx, geno) in enumerate(list_geno):
+        cond = df_VIdate['GENO'] == geno
+        df_geno = df_VIdate.loc[cond,:]  
+        list_loc = df_geno['PLOT_LC']
+        fig = plt.figure(figsize=(10,8))
+        for loc in list_loc:
+            cond1 = df_geno['PLOT_LC'] == loc
+            VI_value = df_geno.loc[cond1, list_dates].astype(datetime).values.tolist()[0]
+            
+            plt.plot(list_dates_, VI_value, '^-', label = loc)
+        plt.title('{} for {}'.format(VI, geno))
+        plt.legend(loc = 'best')
+        pl.xticks(rotation=45)
+        pl.yticks(rotation=45)
+#        pdb.set_trace()
+        plt.savefig(os.path.join(savepath, '{}_{}.png'.format(VI, geno)))
+    plt.close('all')
+#        pdb.set_trace()
+            
+    
+
+
